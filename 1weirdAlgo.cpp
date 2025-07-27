d18585382b026ebe012060a3418c02359cf3e2f4
hahaharshil <hahaharshil@pm.me>
2025-07-27 16:10:26 +0530

    2 more questions

--- 1weirdAlgo.cpp
+++ 1weirdAlgo.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+Submission details
+Task:	Weird Algorithm
+Sender:	hahaharshil
+Submission time:	2025-07-24 15:03:05 +0300
+Language:	C++ (C++17)
+Status:	READY
+Result:	ACCEPTED
+*/
 
 int main(){
 	long long n; cin >> n;
--- 2missingnum.cpp
+++ 2missingnum.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+/* Task:Missing Number
+Sender:	hahaharshil
+Submission time:	2025-07-24 15:11:47 +0300
+Language:	C++ (C++17)
+Status:	READY
+Result:	ACCEPTED */
+
+
 int main(){
 	long long n; cin >> n;
 	int totalsum = (n*(n+1))/2;
